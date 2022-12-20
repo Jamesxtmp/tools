85d410631083c6d4c50fa22d4d5dec66e1635739
JamesCyber <jamesxtmp@gmail.com>
2022-12-19 18:08:34 -0600

    algo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,21 @@
 import Layout from "../components/Layout";
 
+// import "katex/dist/katex.min.css";
+import {BlockMath} from "react-katex";
+
 export default function () {
   return (
     <Layout>
-      Imdex
+      <BlockMath
+        math={`
+          A \\neq 
+          \\begin{pmatrix}
+          2 & 5 & 0\\\\
+          7 & 3 & 8\\\\
+          3 & 0 & 1
+          \\end{pmatrix}
+          `}
+      />
     </Layout>
   );
 }
